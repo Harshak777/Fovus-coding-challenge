3f7496a782ce479e372427007ecd07fe8a7aee60
Harshak Krishnaa <kharshak777@gmail.com>
2023-04-26 00:16:47 -0700

    secondary commit

--- fovus-challenge/src/App.js
+++ fovus-challenge/src/App.js
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+import AWS from 'aws-sdk';
+
+// Configure AWS S3 credentials
+AWS.config.update({
+  accessKeyId: 'AKIARY5SVF3I3NKUN274',
+  secretAccessKey: 'gVFNXkKP770xZ/rjhIGZvQFkQ1YZ/uaZA473a8UW',
+  region: 'us-east-1' // Replace with your desired AWS S3 region
+});
+
 
 const App = () => {
   const [textInput, setTextInput] = useState('');
@@ -10,20 +19,57 @@ const App = () => {
 
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
-    const reader = new FileReader();
+    console.log(event.target.files[0]);
 
-    reader.onload = (e) => {
-      setFileContent(e.target.result);
+    setFileContent(file);
+    // const reader = new FileReader();
+
+    // reader.onload = (e) => {
+    //   setFileContent(e.target.result);
+    // };
+
+    // reader.readAsText(file);
+  }
+
+  const handleFileUpload = (file) => {
+    console.log("file:")
+    console.log(file)
+    // Create an instance of the S3 service
+    const s3 = new AWS.S3();
+  
+    // Set the S3 bucket name and key (file name)
+    const bucketName = 'fovus-challenge-input-bucket';
+    const key = file.name;
+  
+    // Create a new S3 PutObject request
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+      Body: file,
+      ACL: 'public-read' // Set the appropriate ACL for your use case
     };
 
-    reader.readAsText(file);
-  }
+    console.log("came here")
+    console.log(params);
+  
+    // Upload the file to S3
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('File uploaded successfully:', data);
+        // Handle successful upload, e.g., update UI, show success message, etc.
+      }
+    });
+  };
+  
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Handle submit logic here
     console.log('Text Input:', textInput);
     console.log('File Content:', fileContent);
+    handleFileUpload(fileContent);
   }
 
   return (
